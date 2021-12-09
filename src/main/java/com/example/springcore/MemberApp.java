d99af92b207215870a939d966bb64c8cb2523e2b
iiaii <jhun9409@naver.com>
2021-12-09 23:51:09 +0900

    feat: 메인에서 코드 출력 확인

---
+++ src/main/java/com/example/springcore/MemberApp.java
@@ -0,0 +1,19 @@
+package com.example.springcore;
+
+import com.example.springcore.member.Grade;
+import com.example.springcore.member.Member;
+import com.example.springcore.member.MemberService;
+import com.example.springcore.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member);
+        System.out.println("findMember = " + findMember);
+    }
+}
