a6d0cd00c06c8dad01607c70a0a9d3b3b0ece720
iiaii <jhun9409@naver.com>
2021-12-12 22:21:25 +0900

    test, feat: RateDiscountPolicy 테스트 및 구현

--- src/main/java/com/example/springcore/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/springcore/discount/RateDiscountPolicy.java
@@ -3,16 +3,15 @@ package com.example.springcore.discount;
 import com.example.springcore.member.Grade;
 import com.example.springcore.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy {
+public class RateDiscountPolicy implements DiscountPolicy {
 
-    private static final int DISCOUNT_AMOUNT = 1000;
+    private static final int DISCOUNT_PERCENT = 10;
 
     @Override
     public int discount(final Member member, final int price) {
         if (member.isEqualGrade(Grade.VIP)) {
-            return DISCOUNT_AMOUNT;
+            return price * DISCOUNT_PERCENT / 100;
         }
         return 0;
     }
-
 }
--- src/main/java/com/example/springcore/member/Member.java
+++ src/main/java/com/example/springcore/member/Member.java
@@ -12,6 +12,10 @@ public class Member {
         this.grade = grade;
     }
 
+    public boolean isEqualGrade(final Grade grade) {
+        return this.grade == grade;
+    }
+
     public Long getId() {
         return id;
     }
