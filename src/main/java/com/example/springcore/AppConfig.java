58c920c24abcd696ce07ae117a40a0f0fd5b8dbd
iiaii <jhun9409@naver.com>
2021-12-12 23:04:27 +0900

    refactor: AppConfig 메서드 분리 (역할 상세하게 분리)

--- src/main/java/com/example/springcore/AppConfig.java
+++ src/main/java/com/example/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.springcore;
 
+import com.example.springcore.discount.DiscountPolicy;
 import com.example.springcore.discount.FixDiscountPolicy;
+import com.example.springcore.member.MemberRepository;
 import com.example.springcore.member.MemberService;
 import com.example.springcore.member.MemberServiceImpl;
 import com.example.springcore.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.example.springcore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
 }
