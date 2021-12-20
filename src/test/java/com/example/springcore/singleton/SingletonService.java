a3438f200922d6c2f82026565055bc6283375fef
iiaii <jhun9409@naver.com>
2021-12-20 23:56:02 +0900

    test: Singleton 테스트

--- src/test/java/com/example/springcore/singleton/SingletonService.java
+++ src/test/java/com/example/springcore/singleton/SingletonService.java
@@ -1,2 +1,17 @@
-package com.example.springcore.singleton;public class SingletonService {
+package com.example.springcore.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    private SingletonService() {
+    }
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
 }
--- src/test/java/com/example/springcore/singleton/SingletonTest.java
+++ src/test/java/com/example/springcore/singleton/SingletonTest.java
@@ -1,2 +1,42 @@
-package com.example.springcore.singleton;public class SingletonTest {
+package com.example.springcore.singleton;
+
+import com.example.springcore.AppConfig;
+import com.example.springcore.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("순수한 DI 컨테이너 - 매번 다른 객체 생성")
+    public void pureContainer() {
+        // given
+        AppConfig appConfig = new AppConfig();
+
+        // when
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        // then
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        // given
+
+        // when
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        // then
+        assertThat(instance1).isSameAs(instance2);
+    }
+
+
+
 }
