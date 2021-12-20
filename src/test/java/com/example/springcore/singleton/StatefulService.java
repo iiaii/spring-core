68b00bcd64cf294054a4187c3bd3e9dcc4d677c9
iiaii <jhun9409@naver.com>
2021-12-21 00:14:48 +0900

    test: 상태를 가지는 설계의 문제점 테스트

--- src/test/java/com/example/springcore/singleton/StatefulService.java
+++ src/test/java/com/example/springcore/singleton/StatefulService.java
@@ -1,2 +1,16 @@
-package com.example.springcore.singleton;public class StatefulService {
+package com.example.springcore.singleton;
+
+public class StatefulService {
+
+    private int price;
+
+    public void order(final String name, final int price) {
+        System.out.println("name = " + name + " price = " + price);
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
 }
--- src/test/java/com/example/springcore/singleton/StatefulServiceTest.java
+++ src/test/java/com/example/springcore/singleton/StatefulServiceTest.java
@@ -1,2 +1,40 @@
-package com.example.springcore.singleton;public class StatefulServiceTest {
+package com.example.springcore.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StatefulServiceTest {
+
+    @Test
+    @DisplayName("상태를 가지는 싱글톤 테스트")
+    public void statefulServiceSingleton() {
+        // given
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        // when
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        statefulService1.order("userA", 10000);
+        statefulService2.order("userB", 20000);
+
+        // then
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        assertThat(statefulService2.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+
+    }
+
 }
