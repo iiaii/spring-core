556af23f96c7f1631a0f2c085121fbf14f3a0b96
iiaii <jhun9409@naver.com>
2021-12-25 00:05:54 +0900

    test: 생성자 의존관계 주입 우선순위 확인

--- src/main/java/com/example/springcore/AppConfig.java
+++ src/main/java/com/example/springcore/AppConfig.java
@@ -16,25 +16,21 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-        System.out.println("AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
-        System.out.println("AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy() {
-        System.out.println("AppConfig.discountPolicy");
         return new RateDiscountPolicy();
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        System.out.println("AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
--- src/main/java/com/example/springcore/order/OrderServiceImpl.java
+++ src/main/java/com/example/springcore/order/OrderServiceImpl.java
@@ -12,18 +12,6 @@ public class OrderServiceImpl implements OrderService {
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
-    @Autowired
-    public void setMemberRepository(final MemberRepository memberRepository) {
-        System.out.println("OrderServiceImpl.setMemberRepository");
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(final DiscountPolicy discountPolicy) {
-        System.out.println("OrderServiceImpl.setDiscountPolicy");
-        this.discountPolicy = discountPolicy;
-    }
-
     public OrderServiceImpl(final MemberRepository memberRepository, final DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
