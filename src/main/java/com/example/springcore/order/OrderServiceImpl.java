b68ae4878a94cfc5959fe75b33c5a328e030c856
iiaii <jhun9409@naver.com>
2021-12-26 22:52:13 +0900

    docs: 생성자 주입 내용 정리

--- src/main/java/com/example/springcore/order/OrderServiceImpl.java
+++ src/main/java/com/example/springcore/order/OrderServiceImpl.java
@@ -3,14 +3,13 @@ package com.example.springcore.order;
 import com.example.springcore.discount.DiscountPolicy;
 import com.example.springcore.member.Member;
 import com.example.springcore.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(final MemberRepository memberRepository, final DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
