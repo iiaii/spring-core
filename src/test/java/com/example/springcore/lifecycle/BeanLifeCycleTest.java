a3b6ad471c30871bae8baa956b7d1a0d092f645d
iiaii <jhun9409@naver.com>
2021-12-28 22:25:23 +0900

    refactor: InitializingBean, DisposableBean 적용

--- src/test/java/com/example/springcore/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/example/springcore/lifecycle/BeanLifeCycleTest.java
@@ -2,7 +2,6 @@ package com.example.springcore.lifecycle;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
--- src/test/java/com/example/springcore/lifecycle/NetworkClient.java
+++ src/test/java/com/example/springcore/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package com.example.springcore.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(final String url) {
@@ -24,5 +25,16 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
