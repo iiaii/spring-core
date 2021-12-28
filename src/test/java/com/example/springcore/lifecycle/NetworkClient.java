ae77e6ad37ff1e06c70d5c9e444919ce764be667
iiaii <jhun9409@naver.com>
2021-12-28 22:29:59 +0900

    refactor: 빈 등록 초기화, 소멸 메서드 지정하도록 수정

--- src/test/java/com/example/springcore/lifecycle/NetworkClient.java
+++ src/test/java/com/example/springcore/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package com.example.springcore.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -27,14 +27,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
