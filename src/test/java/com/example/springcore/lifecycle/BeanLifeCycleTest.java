19b9f0b4237c62218cd831e21251f8ee579a6d96
iiaii <jhun9409@naver.com>
2021-12-28 22:19:22 +0900

    test: 스프링 빈 라이프 사이클 테스트

--- src/test/java/com/example/springcore/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/example/springcore/lifecycle/BeanLifeCycleTest.java
@@ -1,2 +1,39 @@
-package com.example.springcore.lifecycle;public class BeanLifeCycleTest {
+package com.example.springcore.lifecycle;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    @DisplayName("lifeCycleTest")
+    public void lifeCycleTest() {
+        // given
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+
+        // when
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        // then
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+    }
+
+
 }
--- src/test/java/com/example/springcore/lifecycle/NetworkClient.java
+++ src/test/java/com/example/springcore/lifecycle/NetworkClient.java
@@ -1,2 +1,28 @@
-package com.example.springcore.lifecycle;public class NetworkClient {
+package com.example.springcore.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect = " + url);
+    }
+
+    public void call(final String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
 }
