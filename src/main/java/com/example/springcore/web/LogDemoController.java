a369cc8d21a3cba81cde965d543aa2c12f567a8f
iiaii <jhun9409@naver.com>
2021-12-31 23:43:47 +0900

    docs, refactor: 프록시로 ObjectProvider 대체

--- src/main/java/com/example/springcore/web/LogDemoController.java
+++ src/main/java/com/example/springcore/web/LogDemoController.java
@@ -2,7 +2,6 @@ package com.example.springcore.web;
 
 import com.example.springcore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,13 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(final HttpServletRequest request) {
         String requestUrl = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestUrl);
         myLogger.log("controller test");
         logDemoService.logic("testId");
--- src/main/java/com/example/springcore/web/LogDemoService.java
+++ src/main/java/com/example/springcore/web/LogDemoService.java
@@ -2,17 +2,15 @@ package com.example.springcore.web;
 
 import com.example.springcore.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     public void logic(final String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
